050025da6db4289f73d90980d81334d48a4945e1
shajjad Shagor <shajjadshagor@gmail.com>
2022-11-08 20:54:59 -0800

    all nessery file install and test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Header } from './components';
 
 const App = () => {
   return (
-    <div className='text-2xl'>App</div>
+    <div className='w-screen h-auto flex flex-col'>
+      <Header></Header>
+    </div>
   )
 }
 
