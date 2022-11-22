6f0221d89e35d2af4e1b94d932fa084a6389baae
shajjad Shagor <shajjadshagor@gmail.com>
2022-11-22 04:50:59 -0800

    edit taildind extend and setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Header = () => {
   return (
     <div className='fixed z-50 w-screen bg-slate-300 p-6 px-16'>
-        
+      {/* desktop & tablet */}
+        <div className='hidden md:flex w-full bg-red-600 h-full p-4'>
+
+        </div>
+
+        {/* Mobile */}
+        <div className='flex md:hidden w-full bg-blue-500 h-full p-4'>
+
+        </div>
     </div>
   )
 }
