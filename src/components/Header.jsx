3af9036e4c7f916508d06f37d618bb2f6bd3e7c3
shajjad Shagor <shajjadshagor@gmail.com>
2022-11-22 08:43:41 -0800

    menu set and images add and check it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
+import Logo from './img/logo.png'
 
 const Header = () => {
   return (
-    <div className='fixed z-50 w-screen bg-slate-300 p-6 px-16'>
+    <header className='fixed z-50 w-screen p-6 px-16'>
       {/* desktop & tablet */}
-        <div className='hidden md:flex w-full bg-red-600 h-full p-4'>
+        <div className='hidden md:flex w-full h-full'>
+          <div className='flex items-center gap-2'>
+            <img className='w-8 object-cover' src={Logo} alt="logo" />
+            <p className='text-headingColor text-xl font-bold'>City</p>
+          </div>
 
+          <ul className='flex items-center gap-8 ml-auto'>
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Home</li>
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Menu</li>
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>About Us</li>
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Service</li>
+          </ul>
         </div>
 
         {/* Mobile */}
-        <div className='flex md:hidden w-full bg-blue-500 h-full p-4'>
+        <div className='flex md:hidden w-full  h-full'>
 
         </div>
-    </div>
+    </header>
   )
 }
 
