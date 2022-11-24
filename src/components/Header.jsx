6b2b1ab3d9e8d5ddec45f08829a18ed119d8625d
shajjad Shagor <shajjadshagor@gmail.com>
2022-11-24 05:55:38 -0800

    user login data check and image shw in display and use redux for data collection

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,15 +7,24 @@ import { Link } from 'react-router-dom';
 
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { app } from '../firebase.config'
+import { useStateValue } from '../context/StateProvider';
+import { actionType } from '../context/reducer';
 
 const Header = () => {
 
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
+  const [{user}, dispatch] = useStateValue();
+
+
   const login = async() => {
-    const response = await signInWithPopup(firebaseAuth, provider);
-    console.log(response); 
+    const {user : {refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider);
+    dispatch({
+      type : actionType.SET_USER,
+      user : providerData[0],
+    });
+    localStorage.setItem('user', JSON.stringify(providerData[0])); 
   }
 
   return (
@@ -43,7 +52,7 @@ const Header = () => {
             </div>
 
             <div className='relative'>
-              <motion.img whileTap={{ scale: 0.6 }} src={Avatar} alt="userprofile" className='w-10 min-w-[40px] h-10 min-h-[40px] shadow-2xl drop-shadow-xl cursor-pointer' onClick={login} />
+              <motion.img whileTap={{ scale: 0.6 }} src={user ? user.photoURL : Avatar} alt="userprofile" className='w-10 min-w-[40px] h-10 min-h-[40px] shadow-2xl drop-shadow-xl cursor-pointer rounded-full' onClick={login} />
             </div>
           </div>
         </div>
