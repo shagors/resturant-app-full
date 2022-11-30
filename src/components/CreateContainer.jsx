599ba7176f08fd3e2c0599735159a2e2ca9eba8d
shajjad Shagor <shajjadshagor@gmail.com>
2022-11-29 20:33:33 -0800

    new item page design and section category add and image upload system add and loader spinner add and design it

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,6 +1,8 @@
 import { motion } from 'framer-motion';
 import React, { useState } from 'react';
-import {MdFastfood} from 'react-icons/md'
+import { MdFastfood, MdCloudUpload } from 'react-icons/md'
+import { categories } from '../utils/data';
+import Loader from './Loader';
 
 const CreateContainer = () => {
 
@@ -14,9 +16,13 @@ const CreateContainer = () => {
   const [msg, setMsg] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  const uploadImage = () => {
+    
+  }
+
   return (
     <div className='w-full min-h-screen h-auto flex items-center justify-center'>
-      <div className='w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center'>
+      <div className='w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4'>
           {
             fields && (
               <motion.p 
@@ -40,6 +46,31 @@ const CreateContainer = () => {
               placeholder="Give me a title..."
               className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
             />
+          </div>
+
+          <div className='w-full'>
+            <select onChange={(e) => setCategory(e.target.value)} className='w-full outline-none text-base border-b-2 border-gray-200 rounded-md cursor-pointer'>
+              <option value="other" className='bg-white'>Select Category</option>
+              {categories && categories.map(item => (
+                <option key={item.id} className="text-base border-0 outline-none capitalize bg-white text-headingColor" value={item.urlParamName}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className='group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer rounded-lg'>
+            {isLoading ? <Loader></Loader> : <>
+                {!imageAsset ? <>
+                  <label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+                    <div className='w-full h-full flex flex-col items-center justify-center gap-2'>
+                      <MdCloudUpload className='text-gray-500 text-3xl hover:text-gray-700' />
+                      <p className='text-gray-500 hover:text-gray-700'>Click here to upload</p>
+                    </div>
+                    <input type="file" name='uploadimage' accept='image/*' onChange={uploadImage} />
+                  </label>
+                </> : <></>}
+            </>}
           </div>
       </div>
     </div>
--- src/utils/data.js
+++ src/utils/data.js
@@ -5,9 +5,71 @@ import Fi1 from '../img/fi1.png';
 
 
 export const heroData = [
-    { id: 1, name: 'IceCream', decp: 'Chocolate & Vanilla', price: '5.25', imgSrc: I1 },
-    { id: 2, name: 'Strawberries', decp: 'Fresh Strawberries', price: '7.25', imgSrc: F1 },
-    { id: 3, name: 'Chicken Kebab', decp: 'Mixed Kebab Plate', price: '8.25', imgSrc: C3 },
-    { id: 4, name: 'Fish Kebab', decp: 'Mixed Fish Kebab', price: '8.25', imgSrc: Fi1 },
+    { 
+        id: 1, 
+        name: 'IceCream', 
+        decp: 'Chocolate & Vanilla', 
+        price: '5.25', 
+        imgSrc: I1 
+    },
+    { 
+        id: 2, 
+        name: 'Strawberries', 
+        decp: 'Fresh Strawberries', 
+        price: '7.25', 
+        imgSrc: F1 
+    },
+    { 
+        id: 3, 
+        name: 'Chicken Kebab', 
+        decp: 'Mixed Kebab Plate', 
+        price: '8.25', 
+        imgSrc: C3 
+    },
+    { 
+        id: 4, 
+        name: 'Fish Kebab', 
+        decp: 'Mixed Fish Kebab', 
+        price: '8.25', 
+        imgSrc: Fi1 
+    },
 ];
 
+export const categories = [
+    {
+        id: 1,
+        name: "Chicken",
+        urlParamName: "chicken",
+    },
+    {
+        id: 2,
+        name: "Curry",
+        urlParamName: "curry",
+    },
+    {
+        id: 3,
+        name: "Rice",
+        urlParamName: "rice",
+    },
+    {
+        id: 4,
+        name: "Fish",
+        urlParamName: "fish",
+    },
+    {
+        id: 5,
+        name: "Fruits",
+        urlParamName: "fruits",
+    },
+    {
+        id: 6,
+        name: "Icecreams",
+        urlParamName: "icecreams",
+    },
+
+    {
+        id: 7,
+        name: "Soft Drinks",
+        urlParamName: "drinks",
+    },
+];
