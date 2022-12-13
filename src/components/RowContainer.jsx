e1f972f50c6d382001241c8c313017a5926bfaad
shajjad Shagor <shajjadshagor@gmail.com>
2022-12-13 03:57:09 -0800

    menu card data show and design it and display it

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -3,6 +3,7 @@ import { MdShoppingBasket } from 'react-icons/md';
 import { motion } from 'framer-motion';
 import { useEffect } from 'react';
 import { useRef } from 'react';
+import NotFound from '../img/NotFound.svg';
 
 const RowContainer = ({ flag, data, scrollValue}) => {
     const rowContainer = useRef();
@@ -11,14 +12,16 @@ const RowContainer = ({ flag, data, scrollValue}) => {
     }, [scrollValue]);
   return (
       <div ref={rowContainer} className={`w-full my-12 flex items-center gap-3 scroll-smooth ${flag ? 'overflow-x-scroll scrollbar-none' : 'overflow-x-hidden flex-wrap justify-center'}`}>
-          {data && data.map(item => (
+          {data && data.length > 0 ? data.map(item => (
               <div key={item?.id} className='w-300 min-w-[300px]: md:w-340 md:min-w-[340px] h-auto bg-cardOverlay rounded-lg p-2 my-12 backdrop-blur-lg hover:drop-shadow-xl flex flex-col items-center justify-between'>
                   <div className='w-full flex items-center justify-between'>
-                      <motion.img whileHover={{ scale: 1.2 }} src={item?.imageURL}
-                          alt="" className='w-40 h-[175px] -mt-8 drop-shadow-2xl' />
-                      <motion.div whileTap={{ scale: 0.75 }} className='w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md'>
-                          <MdShoppingBasket className='text-white' />
-                      </motion.div>
+                    <motion.div whileHover={{ scale: 1.2 }} className="w-40 h-40 -mt-8 drop-shadow-2xl">
+                        <img src={item?.imageURL}
+                            alt="" className='w-full h-full object-contain' />
+                    </motion.div>
+                    <motion.div whileTap={{ scale: 0.75 }} className='w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md'>
+                        <MdShoppingBasket className='text-white' />
+                    </motion.div>
                   </div>
 
                   <div className='w-full flex flex-col items-end justify-end'>
@@ -31,7 +34,10 @@ const RowContainer = ({ flag, data, scrollValue}) => {
                       </div>
                   </div>
               </div>
-          ))}
+          )) : <div className='w-full flex flex-col items-center justify-center'>
+                <img src={NotFound} alt='' className='h-340' />
+                <p className='text-xl text-headingColor font-semibold my-2'>Items Not Available</p>
+            </div>}
       </div>
   )
 }
